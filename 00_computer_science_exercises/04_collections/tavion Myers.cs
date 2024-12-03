b06a8922698f4261203ee86c75bd6fa2a8079239
S11788692 <tavionm21@gmail.com>
2024-12-03 13:54:36 -0500

    Update tavion Myers.cs

--- 00_computer_science_exercises/04_collections/tavion Myers.cs
+++ 00_computer_science_exercises/04_collections/tavion Myers.cs
@@ -3,5 +3,18 @@ using System.line;
 Class Morearray {
   staticvoid Main(){
     string[] colors ={"black, purple, red, white, sivler "}
+    for (int  = 0; i < colors.lenght; i++)
+    Console. WriteLine(colors[i]);
+
+
+    foreach (string i in colors)
+
+    console. WriteLine(i);
+
+    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
+
+    foreach(int i in number)
+
+    cosnole. writeLine(i)
   }
 }
